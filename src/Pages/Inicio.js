c094d6685446ff1980b0a612c8791a776c6ce3ca
cc <colauttiadad@gmail.com>
2023-03-23 11:02:22 +0100

    pasé todo a función

--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -1,43 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Noticia from "../Components/Noticia";
 
 
 
-class Inicio extends Component {
-    constructor (props){
-        super(props);
-        this.state = {
-            noticias : [],
-            loading : true
-        }
-    }
+function Inicio (props) {    
 
-    componentDidMount(){
-        fetch("https://jsonplaceholder.typicode.com/posts")
+    const [noticias, setNoticias] = useState([]);
+    const [loading, setLoading] = useState (true);    
+
+    useEffect (
+        ()=>{
+         fetch("https://jsonplaceholder.typicode.com/posts")
         .then(res=>res.json())
-        .then(data=>{
-            console.log("data", data);
-            this.setState({noticias:data});
-            this.setState({loading:false})
+        .then(data=>{console.log("data", data)
+        setNoticias(data)
+        setLoading(false)
+        })},
+        []
+    )
 
-        })
-    }
+    if (loading) {
+        return (
 
-    render(){       
-        
-            return (
-                <div>                    
-               
-                {this.state.noticias.map(n=><Noticia pasamano={n}/>)}
+            <div> 
+                Loading...
+            </div>       
+        ) 
 
+    }else{
+        return (
 
-
-
-                </div>       
-            )           
-                         
-    }   
- 
+            <div> 
+                {noticias.map(n=><Noticia pasamano={n}/>)}
+            </div>       
+        ) 
+    }  
 }
 
 export default Inicio;
