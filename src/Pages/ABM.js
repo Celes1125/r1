d6b29643d3495e9e41692010a44c93ec806cccfa
cc <colauttiadad@gmail.com>
2023-04-08 07:52:23 +0200

    upload and getting products functions were added using Firebase db. One issue is that any product is loading as a product object, and this can be a problem if after it need some importing task, because Firebase dont let you upload new documents as an product object, just let you add the fields. Have this matter in mind!

--- src/Pages/ABM.js
+++ src/Pages/ABM.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from "react";
 import {db} from "../Config/firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, getDocsFromCache, getDocsFromServer} from "firebase/firestore";
 
 
-function ABM (props) {    
+function ABM (props) {   
+ 
 
     const [product, setProduct] = useState({title:"", price:"", description:""});
-        
-    const handleSubmit = async (event)=>{
-        event.preventDefault()
-        try{
-            const responseDocument = await addDoc(collection(db,"products"), {product});
-            console.log("responseDocument: ", responseDocument);
-            
+    const [productList, setProductList] = useState([]);
+   
 
-        }catch(error){
+    
+        
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, "products"), { product });
+            
+            console.log("Entró el handle submit para agregar el producto: ", product);
+
+
+        } catch (error) {
             console.log("error: ", error);
-            alert(error.message)
+            alert(error.message);
 
         }
 
@@ -27,12 +33,58 @@ function ABM (props) {
         const value = event.target.value;
         console.log("entré con el handle change", name, value);
         setProduct ({...product,[name]:value})
-    }    
+    }  
 
+    useEffect (
+        ()=>{
+           const getProductos = async ()=>{
+                try {
+                    const querySnapshot = await getDocs(collection(db, "products"));
+                    setProductList(querySnapshot.docs);    
+                    console.log("PRODUCTOS>>>>>", productList)  
+                    
+                                 
+                } catch (e) {
+                    console.log("error: ", e);
+                    alert(e.message);
+
+                }
+
+            }
+            getProductos();      
+
+        },
+        [productList]
+    )
+    
+    
+    //{productos.map(p=>{p.data().title})};
     
         return (
 
             <div> 
+                <div>
+                    
+                {productList.map(
+                    (doc)=>{
+                        return (
+                           <div key={doc.id}> {doc.data().product.title}</div>
+                        )
+                    }
+                )}
+
+
+                </div>
+
+               
+
+                
+                    
+                
+                
+
+
+                <p>Cargar productos</p>
                 <form onSubmit={handleSubmit}>
                 <div>
                     <label>Título</label>
@@ -47,7 +99,7 @@ function ABM (props) {
                     <input type="text" name="description" value={product.description} onChange={handleChange}></input>
                 </div>
                 <div>
-                    <button type="submit">Submit product</button>
+                    <button type="submit">Submit</button>
                 </div>    
 
             </form>
