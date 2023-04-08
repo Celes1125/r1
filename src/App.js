8a6289a1235dd0a564d16da4bf8f1c382576b341
cc <colauttiadad@gmail.com>
2023-04-08 22:20:17 +0200

    starting from scratch due to the great changes on firebase version

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 
-import './App.css';
-import Inicio from './Pages/Inicio';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Menu from './Components/Menu';
-import Signup from './Pages/Signup';
-import Login from './Pages/Login';
-import DetalleNoticia from './Components/DetalleNoticia';
-import List from './Pages/List';
-import ABM from './Pages/ABM';
-
 
 
 function App() {
 
   return (   
-    <div>
-    
-    <Router>
-      <Menu/>
-    <Routes>     
-    <Route path= "/" exact element={<Inicio/>}/>
-    <Route path= "/noticias/:id" exact element={<DetalleNoticia/>}/>  
-    <Route path= "/signup" exact element={<Signup/>}/>
-    <Route path= "/login" exact element={<Login/>}/>  
-    <Route path= "/list" exact element={<List/>}/>  
-    <Route path= "/abm" exact element={<ABM/>}/>  
-
-       
-    
-    </Routes>  
-    </Router>
-   
+    <div className="App"> 
+        Hola!
 
     </div>     
     
--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,26 +1,29 @@
 
-import { initializeApp } from "firebase/app";
+
+import {initializeApp} from "firebase/app";
 import {getAuth} from 'firebase/auth';
-import {getStorage, RecaptchaVerifier, uploadBytes, getDownloadURL, getBytes} from 'firebase/storage';
-import {getFirestore, collection,addDoc, getDocs,doc,getDoc, query, where, setDoc, deleteDoc} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
+import {getFirestore} from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHx5iA17JKk-NOiIhNsD5gEEO3HwJTFIg",
 
-    authDomain: "react1-25798.firebaseapp.com",
+    apiKey: "AIzaSyAf8z9xKC8inPSCKrl7G1OLU8Bn4kGHETc",
   
-    projectId: "react1-25798",
+    authDomain: "react-1-2b40d.firebaseapp.com",
   
-    storageBucket: "react1-25798.appspot.com",
+    projectId: "react-1-2b40d",
   
-    messagingSenderId: "241811048948",
+    storageBucket: "react-1-2b40d.appspot.com",
   
-    appId: "1:241811048948:web:b63b9b7668632dafbbd1a1" 
+    messagingSenderId: "171752242530",
+  
+    appId: "1:171752242530:web:b6e05af4c52c4e9225c03b"
+  
+  };
+  
 
-};
-
-export const app = initializeApp(firebaseConfig);
+export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
