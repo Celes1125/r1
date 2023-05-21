d8e0cadf7c0481e907f78e69558c5b3275af2ddb
cc <colauttiadad@gmail.com>
2023-05-21 15:59:09 +0200

    I cant move on...but I least can mantein this disaster up to date jajajjaaj

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,47 +1,12 @@
 import React, { useContext, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
-import firebaseApp from '../Config/firebase'
-import { collection, getDocs, deleteDoc } from 'firebase/firestore'
-import { getAuth, deleteUser } from 'firebase/auth'
 import TaskListContext from '../Contexts/TaskListContext'
-
-const auth = getAuth(firebaseApp)
 
 const Profile = () => {
   const context = useContext(TaskListContext)
 
-  const handleDeleteUserCollection = async () => {
-    const confirm = window.confirm('¿Estás seguro que deseas eliminar todos los usuarios?')
-    if (confirm) {
-      try {
-        const querySnapshot = await getDocs(collection(auth, 'users'))
-        if (querySnapshot.exists) {
-          querySnapshot.forEach((doc) => {
-            deleteDoc(doc.ref)
-          })
-          alert('Todos los usuarios han sido eliminados.')
-          console.log('ok')
-        } else {
-          alert('No hay usuarios para eliminar.')
-        }
-      } catch (error) {
-        alert(`Error: ${error}`)
-      }
-    }
-  }
-
   async function handleDeleteCurrentUser () {
-    const confirm = window.confirm('¿Estás seguro que deseas eliminar tu cuenta?')
-    if (confirm) {
-      try {
-        const user = auth.currentUser
-        await deleteUser(user)
-        alert('Su cuenta ha sido eliminada correctamente.')
-        // Agregar aquí la redirección a la página de inicio de sesión o de inicio
-      } catch (error) {
-        alert(`Error: ${error}`)
-      }
-    }
+    alert('user deleted')
   }
 
   useEffect(
@@ -54,11 +19,9 @@ const Profile = () => {
   return (
     <div>
       <h1>Profile</h1>
-      <Button variant='danger' onClick={() => handleDeleteUserCollection()}>
-        Borrar todos los usuarios
-      </Button>
-      <Button variant='warning' onClick={() => handleDeleteCurrentUser()}>
-        Borrar usuario actual
+
+      <Button variant='warning' onClick={ () => handleDeleteCurrentUser() }>
+        Eliminar mi cuenta
       </Button>
     </div>
   )
